dff66fdb893d5131fd502ab49eacfdfa8da54f62
Dmitry <0shadow0@mail.ru>
2017-05-15 14:02:56 +0300

    trace exceptions in net

---
+++ app/src/main/java/ru/kazantsev/template/net/ExceptionResponse.java
@@ -0,0 +1,111 @@
+package ru.kazantsev.template.net;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by 0shad on 14.05.2017.
+ */
+public class ExceptionResponse implements Response {
+
+    final IOException exception;
+
+    public ExceptionResponse(IOException exception) {
+        this.exception = exception;
+    }
+
+    public IOException getException() {
+        return exception;
+    }
+
+    @Override
+    public String getEncoding() {
+        return "UTF-8";
+    }
+
+    @Override
+    public void setEncoding(String encoding) {
+
+    }
+
+    @Override
+    public String getMessage() {
+        return exception.getMessage();
+    }
+
+    @Override
+    public void setMessage(String message) {
+
+    }
+
+    @Override
+    public int getCode() {
+        return 404;
+    }
+
+    @Override
+    public void setCode(int code) {
+
+    }
+
+    @Override
+    public void setArched(boolean b) {
+
+    }
+
+    @Override
+    public boolean isArched() {
+        return false;
+    }
+
+    @Override
+    public boolean isDownloadOver() {
+        return true;
+    }
+
+    @Override
+    public void setDownloadOver(boolean downloadOver) {
+
+    }
+
+    @Override
+    public long length() {
+        return 0;
+    }
+
+    @Override
+    public OutputStream getOutputStream() throws IOException {
+        return null;
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return null;
+    }
+
+    @Override
+    public String getRawContent(String encoding) throws IOException {
+        return getRawContent();
+    }
+
+    @Override
+    public String getRawContent() throws IOException {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        PrintWriter writer = new PrintWriter(stream);
+        exception.fillInStackTrace().printStackTrace(writer);
+        writer.close();
+        return stream.toString();
+    }
+
+    @Override
+    public void setHeaders(Map<String, List<String>> headerFields) {
+
+    }
+
+    @Override
+    public Map<String, List<String>> getHeaders() {
+        return new HashMap<>();
+    }
+}
---
+++ app/src/main/java/ru/kazantsev/template/view/scroller/FastScroller.java
@@ -0,0 +1,78 @@
+package ru.kazantsev.template.view.scroller;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+import ru.kazantsev.template.R;
+import ru.kazantsev.template.view.AdvancedRecyclerView;
+import xyz.danoz.recyclerviewfastscroller.AbsRecyclerViewFastScroller;
+import xyz.danoz.recyclerviewfastscroller.RecyclerViewScroller;
+import xyz.danoz.recyclerviewfastscroller.calculation.VerticalScrollBoundsProvider;
+import xyz.danoz.recyclerviewfastscroller.calculation.position.VerticalScreenPositionCalculator;
+import xyz.danoz.recyclerviewfastscroller.calculation.progress.TouchableScrollProgressCalculator;
+import xyz.danoz.recyclerviewfastscroller.calculation.progress.VerticalLinearLayoutManagerScrollProgressCalculator;
+import xyz.danoz.recyclerviewfastscroller.calculation.progress.VerticalScrollProgressCalculator;
+import xyz.danoz.recyclerviewfastscroller.vertical.VerticalRecyclerViewFastScroller;
+
+/**
+ * Created by 0shad on 14.05.2017.
+ */
+public class FastScroller extends AbsRecyclerViewFastScroller implements RecyclerViewScroller {
+
+    @Nullable
+    private VerticalScrollProgressCalculator mScrollProgressCalculator;
+    @Nullable
+    private VerticalScreenPositionCalculator mScreenPositionCalculator;
+
+    public FastScroller(Context context) {
+        this(context, null);
+    }
+
+    public FastScroller(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FastScroller(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.vertical_recycler_fast_scroller_layout;
+    }
+
+    @Override
+    @Nullable
+    protected TouchableScrollProgressCalculator getScrollProgressCalculator() {
+        return mScrollProgressCalculator;
+    }
+
+    @Override
+    public void moveHandleToPosition(float scrollProgress) {
+        if (mScreenPositionCalculator == null) {
+            return;
+        }
+        mHandle.setY(mScreenPositionCalculator.getYPositionFromScrollProgress(scrollProgress));
+    }
+
+    protected void onCreateScrollProgressCalculator() {
+        VerticalScrollBoundsProvider boundsProvider =
+                new VerticalScrollBoundsProvider(mBar.getY(), mBar.getY() + mBar.getHeight() - mHandle.getHeight());
+        mScrollProgressCalculator = new VerticalScrollProgressCalculator(boundsProvider) {
+
+            float previousPosition = 0;
+
+            @Override
+            public float calculateScrollProgress(RecyclerView recyclerView) {
+                LinearLayoutManager linearLayout = (LinearLayoutManager) recyclerView.getLayoutManager();
+                int first = linearLayout.findFirstVisibleItemPosition();
+                int last = linearLayout.findLastVisibleItemPosition();
+                return (float) first / (recyclerView.getAdapter().getItemCount() - (last - first + 1));
+            }
+        };
+        mScreenPositionCalculator = new VerticalScreenPositionCalculator(boundsProvider);
+    }
+}
