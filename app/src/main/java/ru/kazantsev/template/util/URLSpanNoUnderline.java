e0aee9a60d85849b18f62d49aaae699573fa98d0
Dmitry <0shadow0@mail.ru>
2017-06-04 16:39:14 +0300

    fix pntencial crash

--- app/src/main/java/ru/kazantsev/template/util/URLSpanNoUnderline.java
+++ app/src/main/java/ru/kazantsev/template/util/URLSpanNoUnderline.java
@@ -1,8 +1,15 @@
 package ru.kazantsev.template.util;
 
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Browser;
 import android.text.TextPaint;
 import android.text.style.URLSpan;
+import android.util.Log;
+import android.view.View;
 
 /**
  * Created by 0shad on 31.10.2015.
@@ -20,4 +27,23 @@ public class URLSpanNoUnderline extends URLSpan {
         ds.setUnderlineText(false);
     }
 
+    @Override
+    public void onClick(View widget) {
+        String stringUrl = getURL();
+        if(stringUrl != null) {
+            Context context = widget.getContext();
+            Intent intent;
+            if (stringUrl.contains("@")) {
+                intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + stringUrl));
+            } else {
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse(stringUrl));
+                intent.putExtra(Browser.EXTRA_APPLICATION_ID, context.getPackageName());
+            }
+            try {
+                context.startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                Log.w("URLSpan", "Actvity was not found for intent, " + intent.toString());
+            }
+        }
+    }
 }
