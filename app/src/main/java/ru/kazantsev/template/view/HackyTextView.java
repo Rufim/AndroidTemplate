8572fea0f7b35d5693f36cbeee4cdfe659ab9f61
dmitry <dmitry.kazantsev@constant.obninsk.ru>
2018-01-17 16:15:24 +0300

    new text view

---
+++ app/src/main/java/ru/kazantsev/template/view/HackyTextView.java
@@ -0,0 +1,42 @@
+package ru.kazantsev.template.view;
+
+
+import android.content.Context;
+import android.text.Selection;
+import android.text.Spannable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+// Use this TestView if you use SpannableText with custom LinkMovementMethod and want use select text all in one textView
+public class HackyTextView extends android.support.v7.widget.AppCompatTextView {
+
+    public HackyTextView(Context context) {
+        super(context);
+    }
+
+    public HackyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HackyTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(final MotionEvent event) {
+        // FIXME simple workaround to https://code.google.com/p/android/issues/detail?id=191430
+        int startSelection = getSelectionStart();
+        int endSelection = getSelectionEnd();
+        if (startSelection < 0 || endSelection < 0){
+            Selection.setSelection((Spannable) getText(), getText().length());
+        } else if (startSelection != endSelection) {
+            if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+                final CharSequence text = getText();
+                setText(null);
+                setText(text);
+            }
+        }
+        return super.dispatchTouchEvent(event);
+    }
+}
