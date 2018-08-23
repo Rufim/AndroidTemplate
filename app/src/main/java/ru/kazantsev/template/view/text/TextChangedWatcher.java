2a4c73fa996154472ccc362efc8b0efc7092546c
dmitry <dmitry.kazantsev@constant.obninsk.ru>
2018-08-23 13:57:37 +0300

    fixes

--- app/src/main/java/ru/kazantsev/template/view/text/TextChangedWatcher.java
+++ app/src/main/java/ru/kazantsev/template/view/text/TextChangedWatcher.java
@@ -2,11 +2,24 @@ package ru.kazantsev.template.view.text;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.EditText;
 
 /**
  * Created by Dmitry on 27.04.2016.
  */
 public abstract class TextChangedWatcher implements TextWatcher {
+
+    private EditText editText;
+
+    public TextChangedWatcher(){}
+
+    public TextChangedWatcher(EditText editText) {
+        this.editText = editText;
+    }
+
+    public EditText getEditText() {
+        return editText;
+    }
 
     public abstract void textChanged(Editable editable);
 
